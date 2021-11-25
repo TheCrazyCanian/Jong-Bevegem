83b9a7193b343504d7f80d8bf970b311c32b5d0f
Gilles De Praeter <gilles.depraeter@student.hogent.be>
2021-11-25 13:59:13 +0100

    python code aangepast, totaalFile toegeoegd!

--- Python website/svdw.py
+++ Python website/svdw.py
@@ -30,6 +30,20 @@ with open('Speler Van De Week.csv', 'r') as infile:
         namen = dict(sorted(namen.items(), key=lambda item: item[1]))
         lineCount -= 1
         for k, v in namen.items():
-            writer.writerow([k, ('%.2f' % (v / lineCount))])
+            namen[k] = ('%.2f' % (v / lineCount))
+            writer.writerow([k, namen[k]])
         writer.writerow('')
         writer.writerow(['Aantal Personen die stemden', lineCount])
+
+with open('SVDWTotaal.csv') as infile:
+    reader = csv.reader(infile)
+    oudTotaal = {row[0]: row[1] for idx,
+                 row in enumerate(reader) if idx in range(0, 25)}
+
+with open('SVDWTotaal.csv', 'w', newline='') as outfile:
+    writer = csv.writer(outfile, delimiter=',')
+    for i, j in oudTotaal.items():
+        oudTotaal[i] = float(j) + float(namen[i])
+    oudTotaal = dict(sorted(oudTotaal.items(), key=lambda item: item[1]))
+    for i, j in oudTotaal.items():
+        writer.writerow([i, ('%.2f' % j)])
