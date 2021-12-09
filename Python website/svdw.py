730ac1b0d1fe5ce42e89bf71f3e88b3c746a4445
Gilles De Praeter <gilles.depraeter@student.hogent.be>
2021-12-09 20:41:56 +0100

    python aangepast, ; toegevoegd als delimiter

--- Python website/svdw.py
+++ Python website/svdw.py
@@ -9,7 +9,7 @@ with open('Speler Van De Week.csv', 'r') as infile:
     filename = ('SVDW%s_%s_%s.csv' % (datetime.now().strftime('%d'),
                 datetime.now().strftime('%m'), datetime.now().strftime('%Y')))
     with open(filename, 'w', newline='') as outfile:
-        writer = csv.writer(outfile, delimiter=',')
+        writer = csv.writer(outfile, delimiter=';')
         for row in reader:
             if lineCount == 0:
                 writer.writerow(
@@ -40,11 +40,11 @@ with open('Speler Van De Week.csv', 'r') as infile:
 
 if exists('SVDWTotaal.csv'):
     with open('SVDWTotaal.csv') as infile:
-        reader = csv.reader(infile)
+        reader = csv.reader(infile, delimiter=';')
         oudTotaal = {row[0]: row[1] for idx,
                      row in enumerate(reader) if idx in range(0, 24)}
     with open('SVDWTotaal.csv', 'w', newline='') as outfile:
-        writer = csv.writer(outfile, delimiter=',')
+        writer = csv.writer(outfile, delimiter=';')
         for i, j in oudTotaal.items():
             oudTotaal[i] = float(j) + float(namen[i])
         oudTotaal = dict(sorted(oudTotaal.items(), key=lambda item: item[1]))
@@ -52,7 +52,7 @@ if exists('SVDWTotaal.csv'):
             writer.writerow([i, ('%.2f' % j)])
 else:
     with open('SVDWTotaal.csv', 'w', newline='') as outfile:
-        writer = csv.writer(outfile, delimiter=',')
+        writer = csv.writer(outfile, delimiter=';')
         beginTotaal = {"Anton Vanhauwere": 0, "Dries Hofman": 0, "Dries Uytterhaegen": 0, "Emiel van Wetter": 0, "Gilles Speltincx": 0, "Isaak Noerens": 0, "Jari Verhecken": 0, "Jens De Temmerman": 0, "Jens Lateur": 0, "Jona Vangansbeke": 0, "Lucas Ardyns": 0, "Maarten Van Snick": 0, "Marcel Machiels": 0,
                        "Mathias Schepers": 0, "Matthias Callebaut": 0, "Matthieu De Pauw": 0, "Mauritz Carlier": 0, "Miel Herreman": 0, "Nisse Verstuyft": 0, "Reggie Demoor": 0, "Sem Backaert": 0, "Thomas Schepers": 0, "Tim Notebaert": 0, "Wout Van der Meulen": 0}
         for i, j in beginTotaal.items():
